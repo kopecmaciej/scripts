8cdf09675baf56541d23d0719ced8c2a2ebcac61
Maciej Kopeć <maciejkopec92@gmail.com>
2022-04-16 19:17:40 +0200

    feat: add go app for test purposes

--- upgrade-mongo/test-go-app/main.go
+++ upgrade-mongo/test-go-app/main.go
@@ -1,10 +1,74 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
 
-type Person struct {
-}
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var mongoUri string = "mongodb://localhost:27017,localhost:27018,localhost:27019/db-test?replicaSet=rs0"
+
+var ctx = context.Background()
 
 func main() {
-	fmt.Println("Starting app")
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoUri))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer client.Disconnect(ctx)
+
+	db := client.Database("db-test")
+
+	err = db.CreateCollection(ctx, "coll_test")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	testColl := db.Collection("coll_test")
+
+	defer testColl.Drop(ctx)
+
+	for i := 0; i < 1000; i++ {
+
+		insertValue(testColl, i)
+		time.Sleep(2 * time.Second)
+
+	}
+}
+
+func insertValue(collection *mongo.Collection, value int) {
+	res, err := collection.InsertOne(ctx, bson.D{{Key: "count", Value: value}})
+
+	var _id interface{}
+
+	if res != nil {
+		_id = res.InsertedID
+	} else {
+		res, err := collection.InsertOne(ctx, bson.D{{Key: "count", Value: value}})
+
+		if err != nil {
+			log.Fatal("Something go wrong with inserting document")
+			return
+		}
+		_id = res.InsertedID
+
+	}
+	filter := bson.D{{Key: "_id", Value: _id}}
+
+	var result bson.D
+	err = collection.FindOne(ctx, filter).Decode(&result)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(result)
 }
