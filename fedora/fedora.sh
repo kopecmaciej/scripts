0db21b8f3c2fb3346ee906965d10764551980646
maciejkopec <maciejkopec92@gmail.com>
2022-12-17 21:37:38 +0100

    chore: add if statements

--- fedora/fedora.sh
+++ fedora/fedora.sh
@@ -23,6 +23,9 @@ fisher install jorgebucaran/nvm.fish
 fi
 
 # docker
+if test -f "/usr/bin/docker" ; then
+    echo "docker is already installed"
+else
 sudo dnf -y install dnf-plugins-core
 sudo dnf -y config-manager \
     --add-repo \
@@ -32,11 +35,19 @@ sudo systemctl start docker
 sudo systemctl enable docker
 sudo usermod -aG docker $USER
 sudo newgrp docker
+fi
 
 #docker-compose
+if test -f "/usr/bin/docker-compose" ; then
+    echo "docker-compose is already installed"
+else
 sudo dnf -y install docker-compose
- 
+fi
+
 # kubectl
+if test -f "/usr/bin/kubectl" ; then
+    echo "kubectl is already installed"
+else
 cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
@@ -46,8 +57,26 @@ gpgcheck=1
 gpgkey=https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 EOF
 sudo yum install -y kubectl
+fi
 
 #tabby 
+if test -f "/usr/bin/tabby" ; then
+    echo "tabby is already installed"
+else
 wget https://github.com/Eugeny/tabby/releases/download/v1.0.187/tabby-$tabbyVersion-linux-x64.rpm
 sudo rpm -i tabby-$tabbyVersion-linux-x64.rpm
 rm tabby-$tabbyVersion-linux-x64.rpm
+fi
+
+### Browsers
+
+# brave
+
+if test -f "/usr/bin/brave-browser" ; then
+    echo "brave is already installed"
+else
+sudo dnf -y install dnf-plugins-core
+sudo dnf -y config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
+sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+sudo dnf -y install brave-browser
+fi
