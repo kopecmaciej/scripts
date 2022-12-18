242a21141885546967e1615d5c3b46dd4ccb8a59
maciejkopec <maciejkopec92@gmail.com>
2022-12-18 15:42:08 +0100

    add font

--- fedora/fedora.sh
+++ fedora/fedora.sh
@@ -1,61 +1,59 @@
 #!/bin/bash
 
 tabbyVersion=1.0.187
+slackVersion=4.29.149
+sopsVersion=3.7.3
 
 #dnf
-if cat /etc/dnf/dnf.conf | grep -q "defaultyes=True" ; then
+if cat /etc/dnf/dnf.conf | grep -q "defaultyes=True"; then
     echo "defaultyes is already enabled"
 else
-echo 'defaultyes=True' >> /etc/dnf/dnf.conf
+    echo 'defaultyes=True' >>/etc/dnf/dnf.conf
 fi
 
 # fish
-if test -f "/usr/bin/fish" ; then
+if test -f "/usr/bin/fish"; then
     echo "fish is already installed"
-else 
-sudo dnf -y install fish
-sudo chsh -s /usr/bin/fish
-
-#oh-my-fish
-curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install
-
-#fisher
-curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
-
-#bass
-fisher install edc/bass
-
-#nvm
-fisher install jorgebucaran/nvm.fish
+else
+    sudo dnf -y install fish
+    sudo chsh -s /usr/bin/fish
+    #oh-my-fish
+    echo "
+        run follow command manually:
+        curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+        curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+        fisher install edc/bass
+        fisher install jorgebucaran/nvm.fish
+    "
 fi
 
 # docker
-if test -f "/usr/bin/docker" ; then
+if test -f "/usr/bin/docker"; then
     echo "docker is already installed"
 else
-sudo dnf -y install dnf-plugins-core
-sudo dnf -y config-manager \
-    --add-repo \
-    https://download.docker.com/linux/fedora/docker-ce.repo
-sudo dnf -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
-sudo systemctl start docker
-sudo systemctl enable docker
-sudo usermod -aG docker $USER
-sudo newgrp docker
+    sudo dnf -y install dnf-plugins-core
+    sudo dnf -y config-manager \
+        --add-repo \
+        https://download.docker.com/linux/fedora/docker-ce.repo
+    sudo dnf -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+    sudo systemctl start docker
+    sudo systemctl enable docker
+    sudo usermod -aG docker $USER
+    sudo newgrp docker
 fi
 
 #docker-compose
-if test -f "/usr/bin/docker-compose" ; then
+if test -f "/usr/bin/docker-compose"; then
     echo "docker-compose is already installed"
 else
-sudo dnf -y install docker-compose
+    sudo dnf -y install docker-compose
 fi
 
 # kubectl
-if test -f "/usr/bin/kubectl" ; then
+if test -f "/usr/bin/kubectl"; then
     echo "kubectl is already installed"
 else
-cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+    cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
 baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
@@ -63,27 +61,56 @@ enabled=1
 gpgcheck=1
 gpgkey=https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 EOF
-sudo yum install -y kubectl
+    sudo yum install -y kubectl
 fi
 
-#tabby 
-if test -f "/usr/bin/tabby" ; then
+#tabby
+if test -f "/usr/bin/tabby"; then
     echo "tabby is already installed"
 else
-wget https://github.com/Eugeny/tabby/releases/download/v1.0.187/tabby-$tabbyVersion-linux-x64.rpm
-sudo rpm -i tabby-$tabbyVersion-linux-x64.rpm
-rm tabby-$tabbyVersion-linux-x64.rpm
+    wget https://github.com/Eugeny/tabby/releases/download/v1.0.187/tabby-$tabbyVersion-linux-x64.rpm
+    sudo rpm -i tabby-$tabbyVersion-linux-x64.rpm
+    rm tabby-$tabbyVersion-linux-x64.rpm
 fi
 
 ### Browsers
 
 # brave
-
-if test -f "/usr/bin/brave-browser" ; then
+if test -f "/usr/bin/brave-browser"; then
     echo "brave is already installed"
 else
-sudo dnf -y install dnf-plugins-core
-sudo dnf -y config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
-sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
-sudo dnf -y install brave-browser
+    sudo dnf -y install dnf-plugins-core
+    sudo dnf -y config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
+    sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+    sudo dnf -y install brave-browser
+fi
+
+# slack
+if test -f "/usr/bin/slack"; then
+    echo "slack is already installed"
+else
+    wget https://downloads.slack-edge.com/releases/linux/$slackVersion/prod/x64/slack-$slackVersion-0.1.el8.x86_64.rpm
+    sudo rpm -i slack-$slackVersion-0.1.el8.x86_64.rpm
+    rm slack-$slackVersion-0.1.el8.x86_64.rpm
+fi
+
+#sops
+if test -f "/usr/local/bin/sops"; then
+    echo "sops is already installed"
+else
+    wget https://github.com/mozilla/sops/releases/download/v$sopsVersion/sops-$sopsVersion-1.x86_64.rpm
+    sudo rpm -i sops-$sopsVersion-1.x86_64.rpm
+    rm sops-$sopsVersion-1.x86_64.rpm
+fi
+
+#check if droid sans mono nerd font is installed
+if test -e "/usr/share/fonts/droid-sans-mono-nerd-font"; then
+    echo "droid sans mono nerd font is already installed"
+else
+    wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/DroidSansMono.zip
+    sudo mkdir /usr/share/fonts/droid-sans-mono-nerd-font
+    unzip -d DroidSansMono DroidSansMono.zip
+    sudo mv DroidSansMono/* /usr/share/fonts/droid-sans-mono-nerd-font
+    fc-cache -f
+    rm -rf DroidSansMono.zip DroidSansMono
 fi
